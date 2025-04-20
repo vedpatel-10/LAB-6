a2e37b53b4fbe4c85d4c48a0f016c1ce584f09c8
vedpatel-10 <24bee209@spt.pdpu.ac.in>
2025-04-20 13:02:30 +0530

    Update Q-4.py

--- Q-4.py
+++ Q-4.py
@@ -2,3 +2,6 @@ import operator
 menu = [("Pizza",120),("Burger",80),("Coffee", 50),("Lemonade",30),("Sandwich",50)]
 
 print(sorted(menu, key = operator.itemgetter(1), reverse=True))
+
+#OUTPUT:
+# [('Pizza', 120), ('Burger', 80), ('Coffee', 50), ('Sandwich', 50), ('Lemonade', 30)]
