2aaabe6be3c1d7b1a5f74990e5b5ec3263cc65e2
vedpatel-10 <24bee209@spt.pdpu.ac.in>
2025-04-20 13:03:33 +0530

    Update Q-7.py

--- Q-7.py
+++ Q-7.py
@@ -2,3 +2,6 @@ tpl = (1,23,32,45,441,21)
 i = 2                                        # want to remove 32 from tuple
 new_tpl = tpl[0:i] + tpl[3:]
 print(new_tpl)
+
+#OUTPUT:
+# (1, 23, 45, 441, 21)
