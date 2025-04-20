ea04ee67a5ba4a93e477f922afce6e70b2a03255
vedpatel-10 <24bee209@spt.pdpu.ac.in>
2025-04-20 13:03:14 +0530

    Update Q-6.py

--- Q-6.py
+++ Q-6.py
@@ -1,3 +1,6 @@
 original_tuple = (1, 2, 3, 4)
 modified_tuple = original_tuple[:2] + (99,) + original_tuple[3:]
 print("Modified tuple:", modified_tuple)
+
+#OUTPUT:
+# Modified tuple: (1, 2, 99, 4)
